0e8436bfa92465651e5a2255d6468cfb78d33679
Sara Simnica <Sarasimnica@Outlook.com>
2025-11-22 14:16:41 +0100

    update

--- resources/views/partials/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="text-center text-white py-4 mt-5">
-    <div class="container">
-        <p>&copy; {{ date('Y') }} City Care. All rights reserved.</p>
-    </div>
-</footer>
-
--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\MapController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/map/issues', [MapController::class, 'issues'])->name('api.map.issues');
-Route::get('/map/heatmap', [MapController::class, 'heatmap'])->name('api.map.heatmap');
-
