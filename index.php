e566b2a156806d18bf62cdc6df43aadec8837a30
sara04-05 <Sarasimnica@outlook.com>
2025-11-22 16:22:29 +0100

    update

--- index.php
+++ index.php
@@ -199,12 +199,14 @@ body::before {
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto" id="navMenu">
                     <?php if (!isLoggedIn()): ?>
-                        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="about.php">About Us</a></li>
-                        <li class="nav-item"><a class="nav-link" href="contact.php">Contact Us</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#" onclick="openLoginModal(); return false;">Login</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#" onclick="openSignupModal(); return false;">Sign Up</a></li>
-                    <?php else: ?>
+    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+    <li class="nav-item"><a class="nav-link" href="about.php">About Us</a></li>
+    <li class="nav-item"><a class="nav-link" href="contact.php">Contact Us</a></li>
+    <li class="nav-item"><a class="nav-link" href="blog.php">Make Your City Better</a></li> <!-- added -->
+    <li class="nav-item"><a class="nav-link" href="#" onclick="openLoginModal(); return false;">Login</a></li>
+    <li class="nav-item"><a class="nav-link" href="#" onclick="openSignupModal(); return false;">Sign Up</a></li>
+<?php else: ?>
+
                         <li class="nav-item"><a class="nav-link" href="index.php">Home (Map)</a></li>
                         <li class="nav-item"><a class="nav-link" href="report.php">Report an Issue</a></li>
                         <li class="nav-item"><a class="nav-link" href="blog.php">Make Your City Better</a></li>
